ee454c400ad4b7e23c93222b852bb59e96331bb9
Mustafa Mustafa <mr.m.mustafa@gmail.com>
2014-01-10 08:19:38 -0500

    StMcPxlHit has local sensor coordinates instead of global coordinates

--- HFT/code_dev/StRoot/StPxlSimMaker/StPxlSimMaker.h
+++ HFT/code_dev/StRoot/StPxlSimMaker/StPxlSimMaker.h
@@ -1,11 +1,15 @@
 /*
- * $Id: $
+ * $Id: StPxlSimMaker.h,v 1.1 2013/05/12 21:43:33 jeromel Exp $
  *
- * Author: A. Rose, LBL, Y. Fisyak, BNL, M. Miller, MIT, M. Mustafa
+ * Author: M. Mustafa
  *
  * 
  **********************************************************
- * $Log: $
+ * $Log: StPxlSimMaker.h,v $
+ * Revision 1.1  2013/05/12 21:43:33  jeromel
+ * Initial revision, code peer review closed 2013/05/06
+ *
+ * Revision 1.4  2013/05/03 15:08:19  mstftsm
  *
  */
 
@@ -63,7 +67,7 @@ class StPxlSimMaker : public StMaker
    *  of all maker versions in use.
   */
   virtual const char *GetCVS() const
-  {static const char cvs[]="Tag $Name$ $Id$ built "__DATE__" "__TIME__ ; return cvs;}
+  {static const char cvs[]="Tag $Name:  $ $Id: StPxlSimMaker.h,v 1.1 2013/05/12 21:43:33 jeromel Exp $ built "__DATE__" "__TIME__ ; return cvs;}
 
 private:
     StPxlISim* mPxlSimulator;
@@ -77,52 +81,15 @@ private:
 #endif
 
 /*
- * $Id: $
- *
- * Author: A. Rose, LBL, Y. Fisyak, BNL, M. Miller, MIT, M. Mustafa
+ * $Id: StPxlSimMaker.h,v 1.1 2013/05/12 21:43:33 jeromel Exp $
  *
  * 
  **********************************************************
- * $Log: StPixelFastSimMaker.h,v $
- * Revision 1.13  2009/02/06 20:48:48  wleight
- * UPGR15 Update
+ * $Log: StPxlSimMaker.h,v $
+ * Revision 1.1  2013/05/12 21:43:33  jeromel
+ * Initial revision, code peer review closed 2013/05/06
  *
- * Revision 1.12  2009/01/26 14:50:46  fisyak
- * Clean up
- *
- * Revision 1.11  2007/11/06 16:20:07  wleight
- * Digitized Pixel, removed all hit smearing, and implemented idTruth
- *
- * Revision 1.10  2007/10/18 14:25:13  didenko
- * updates for pile-up events
- *
- * Revision 1.9  2007/09/09 17:00:33  fisyak
- * Fix bug 1056
- *
- * Revision 1.8  2007/04/23 18:11:48  andrewar
- * Removed references to Hpd (includes were obsolete)
- *
- * Revision 1.7  2007/04/16 19:10:52  wleight
- * Added IST simulation (digitization but no clustering)
- *
- * Revision 1.6  2007/04/06 14:55:33  andrewar
- * Shift of HFT to face of ladder.
- *
- * Revision 1.5  2006/12/15 02:17:20  wleight
- * Ist now gets hit smearing parameters from the database
- *
- * Revision 1.4  2006/12/14 23:52:52  andrewar
- * Added Sevil's hit error db loader.
- *
- * Revision 1.3  2006/11/29 21:42:20  andrewar
- * Update with Pixel resolution smearing.
- *
- * Revision 1.2  2006/11/28 21:29:13  wleight
- * Added smearing for Hpd and Ist and a switch to turn it on and off
- *
- * Revision 1.1  2006/02/03 20:11:57  fisyak
- * The initial revision
- *
+ * Revision 1.4  2013/05/03 15:08:19  mstftsm
  *
  */
 
